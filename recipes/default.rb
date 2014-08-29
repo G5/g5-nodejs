b6b639cc0ca859417002cdb2092c094d60a08138
maeve <maeve.revels@getg5.com>
2014-08-29 14:29:37 -0700

    Move npm prefix to vagrant home dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,17 @@
 include_recipe 'nodejs::default'
 
+execute 'Configure npm prefix' do
+  command 'npm config set prefix /home/vagrant/.node --global'
+end
+
+template '/etc/profile.d/npm.sh' do
+  source 'npm.sh.erb'
+  mode 0640
+  owner 'root'
+  group 'vagrant'
+#  variables()
+end
+
 nodejs_npm 'ember-cli' do
   group 'vagrant'
 end
@@ -8,12 +20,12 @@ nodejs_npm 'bower' do
   group 'vagrant'
 end
 
-execute 'Set node_modules directory group permissions' do
-  command 'chmod -R 2775 /usr/lib/node_modules'
+execute 'Set prefix directory group permissions' do
+  command 'chmod -R 2775 /home/vagrant/.node'
 end
 
-execute 'Set node_modules directory ownership' do
-  command 'chown -R nobody:vagrant /usr/lib/node_modules'
+execute 'Set prefix directory ownership' do
+  command 'chown -R nobody:vagrant /home/vagrant/.node'
 end
 
 execute 'Clear npm cache' do
--- test/unit/recipes/default_spec.rb
+++ test/unit/recipes/default_spec.rb
@@ -9,6 +9,10 @@ describe 'g5-nodejs::default' do
     expect(chef_run).to include_recipe('nodejs::default')
   end
 
+  it 'configures the npm prefix' do
+    expect(chef_run).to run_execute('npm config set prefix /home/vagrant/.node --global')
+  end
+
   it 'installs the ember-cli node package' do
     expect(chef_run).to install_nodejs_npm('ember-cli')
   end
@@ -17,12 +21,16 @@ describe 'g5-nodejs::default' do
     expect(chef_run).to install_nodejs_npm('bower')
   end
 
-  it 'makes the node_modules directory writable by the group' do
-    expect(chef_run).to run_execute('chmod -R 2775 /usr/lib/node_modules')
+  it 'makes the prefix directory writable by the group' do
+    expect(chef_run).to run_execute('chmod -R 2775 /home/vagrant/.node')
   end
 
-  it 'assigns the node_modules directory to the vagrant group' do
-    expect(chef_run).to run_execute('chown -R nobody:vagrant /usr/lib/node_modules')
+  it 'assigns the prefix directory to the vagrant group' do
+    expect(chef_run).to run_execute('chown -R nobody:vagrant /home/vagrant/.node')
+  end
+
+  it 'creates the /etc/profile.d/npm.sh template' do
+    expect(chef_run).to create_template('/etc/profile.d/npm.sh')
   end
 
   it 'clears the npm cache' do
